75ca85968d661f88a3b6b453494dd3a9ad5fcb0d
Jipos <nonoma@hotmail.com>
2017-07-16 20:36:52 +0200

    Add CRUD operations to controller and service, and secure them with custom PermissionEvaluator

--- src/main/kotlin/be/kuleuven/toledo/tolbizzv2/TolBizzV2Application.kt
+++ src/main/kotlin/be/kuleuven/toledo/tolbizzv2/TolBizzV2Application.kt
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.security.access.PermissionEvaluator
+import org.springframework.security.access.prepost.PostAuthorize
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.authentication.ProviderManager
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService
@@ -14,15 +20,18 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import java.security.Principal
+import java.io.Serializable
 import java.util.*
+import java.util.function.Predicate
 
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 class TolBizzV2Application {
 
     @Bean
@@ -41,9 +50,15 @@ class TolBizzV2Application {
 }
 
 class UsernameOnlyUserDetailsService : UserDetailsService {
-    override fun loadUserByUsername(username: String?) =
-            User("$username", "", listOf())
 
+    override fun loadUserByUsername(username: String?) =
+            User("$username", "", listOf(MyGrantedAuthority("USER")))
+
+}
+
+class MyGrantedAuthority(@JvmField val authority: String): GrantedAuthority {
+    override fun getAuthority() = authority
+    override fun toString() = authority
 }
 
 fun main(args: Array<String>) {
@@ -56,17 +71,86 @@ class MyController @Autowired constructor(val service: MyService) {
     @GetMapping("/user/{id}")
     fun getUser(@PathVariable id: String) = service.findById(id)
 
+    @GetMapping("/authorities")
+    fun getAuthorities(@AuthenticationPrincipal principal: User): List<String> {
+        println("actor=${principal.authorities}")
+        return principal.authorities.map { it.authority }
+    }
+
+    @GetMapping("/create")
+    fun create(): Link = service.create(NewLink("foobar"))
+
+    @GetMapping("/update")
+    fun update(): Link = service.update(Link("id", "name"))
+
+    @GetMapping("/delete")
+    fun delete(): Link = service.delete(Link("id", "name"))
+
+    @GetMapping("/get")
+    fun get(): Link = service.get("id")
 }
 
-fun principal(): Optional<Principal> = Optional.ofNullable(SecurityContextHolder.getContext().authentication)
+fun principal(): Optional<Authentication> = Optional.ofNullable(SecurityContextHolder.getContext().authentication)
 
 @Service
 class MyService {
 
     fun findById(id: String): Map<String, String> {
         val principal = principal()
-        println("id=$id; actor=${principal.map { it.name }.orElse("unknown")}")
+        println("id=$id; actor=${principal.map { "${it.name} -> ${it.authorities}" }.orElse("unknown")}")
         return mapOf("id" to id)
     }
+
+    @PreAuthorize("hasPermission(#link, 'create')")
+    fun create(link: NewLink): Link = Link("TODO", link.name)
+
+    @PreAuthorize("hasPermission(#link, 'update')")
+    fun update(link: Link): Link = link
+
+    @PreAuthorize("hasPermission(#link, 'delete')")
+    fun delete(link: Link): Link = link
+
+    @PostAuthorize("hasPermission(#returnObject, 'read')")
+    fun get(id: String): Link = Link(id, "TODO")
+
+}
+
+open class NewLink(val name: String)
+class Link(val id: String, name: String): NewLink(name)
+
+@Component
+class MyPermissionEvaluator: PermissionEvaluator {
+
+    override fun hasPermission(authentication: Authentication?, targetId: Serializable?, targetType: String?, permission: Any?): Boolean {
+        throw UnsupportedOperationException("Only targetDomainObject based permission checks are supported.")
+    }
+
+    override fun hasPermission(authentication: Authentication?, targetDomainObject: Any?, permission: Any?): Boolean {
+        println("hasPermission? $authentication - $targetDomainObject - $permission")
+        return true
+    }
+}
+
+
+interface PollablePermissionEvaluator: PermissionEvaluator, Predicate<Any>
+
+class CompositePermissionEvaluator @Autowired constructor(val evaluators: List<PollablePermissionEvaluator>): PermissionEvaluator {
+
+    override fun hasPermission(authentication: Authentication?, targetId: Serializable?, targetType: String?, permission: Any?): Boolean {
+        throw UnsupportedOperationException("Only targetDomainObject based permision checks are supported.")
+    }
+
+    override fun hasPermission(authentication: Authentication?, targetDomainObject: Any?, permission: Any?) =
+        if (targetDomainObject == null) {
+            false
+        } else {
+            evaluators.any { evaluator ->
+                if (evaluator.test(targetDomainObject)) {
+                    evaluator.hasPermission(authentication, targetDomainObject, permission);
+                } else {
+                    false
+                }
+            }
+        }
 
 }
